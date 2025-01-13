b3e29fab65891ac81a27dabbd2f26a19837ad4d5
BryanChow0112 <120325403+BryanChow0112@users.noreply.github.com>
2025-01-13 20:12:00 +0800

    Implement core calculator operations and operator logic
    - Create basic math functions (add, subtract, multiply, divide)
    - Add variables to store operation components (numbers and operator)
    - Implement operate function to handle calculations
    - Test operations in browser console

---
+++ script.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+let firstNumber = "";
+let secondNumber = "";
+let currentOperation = null;
+
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "−":
+      return substract(a, b);
+    case "×":
+      return multiply(a, b);
+    case "÷":
+      if (b === 0) return null;
+      else return divide(a, b);
+    default:
+      return null;
+  }
+}
